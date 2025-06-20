27d5ae58852b293266981b0b2bc40a7d711d6ef8
Filipe Kisiolar <filipekisiolarfilho@terra.com.br>
2025-06-20 19:13:13 -0300

    feat: add endpoints to create, add, and delete HorariosPorDia in CompromissosRecorrentesController

--- src/main/java/kisiolar/filipe/Viviane/Ai/CompromissosRecorrentes/CompromissosRecorrentesController.java
+++ src/main/java/kisiolar/filipe/Viviane/Ai/CompromissosRecorrentes/CompromissosRecorrentesController.java
@@ -1,15 +1,16 @@
 package kisiolar.filipe.Viviane.Ai.CompromissosRecorrentes;
 
-import kisiolar.filipe.Viviane.Ai.CompromissosRecorrentes.DTOs.DTOCompromissosRecorrentes;
-import kisiolar.filipe.Viviane.Ai.CompromissosRecorrentes.DTOs.DTORespostaCompromissoRecorrente;
-import kisiolar.filipe.Viviane.Ai.CompromissosRecorrentes.DTOs.DTORespostasListasCompromissoRecorrentes;
-import kisiolar.filipe.Viviane.Ai.CompromissosRecorrentes.DTOs.DTOUpdateCompromissosRecorrentes;
+import kisiolar.filipe.Viviane.Ai.Compromissos.DTOs.DTOSaidaCompromissos;
+import kisiolar.filipe.Viviane.Ai.CompromissosRecorrentes.DTOs.CompromissosRecorrentes.*;
+import kisiolar.filipe.Viviane.Ai.CompromissosRecorrentes.DTOs.HorariosPorDia.DTOCreateHorariosPorDia;
+import kisiolar.filipe.Viviane.Ai.CompromissosRecorrentes.DTOs.HorariosPorDia.DTOSaidaHorariosPorDia;
+import kisiolar.filipe.Viviane.Ai.CompromissosRecorrentes.DTOs.HorariosPorDia.DTOUpdateHorariosPorDia;
+import kisiolar.filipe.Viviane.Ai.CompromissosRecorrentes.HorariosPorDia.HorariosPorDiaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.time.DayOfWeek;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -20,6 +21,9 @@ public class CompromissosRecorrentesController {
 
     @Autowired
     private CompromissosRecorrentesService compromissosRecorrentesService;
+
+    @Autowired
+    private HorariosPorDiaService horariosPorDiaService;
 
     @GetMapping("/listarcompromissos")
     public ResponseEntity<?> listarCompromissos(){
@@ -48,34 +52,53 @@ public class CompromissosRecorrentesController {
         return ResponseEntity.ok(dtoCompromissosRecorrentes);
     }
 
-    @GetMapping("/listarcompromissosdiadasemana/{diadasemana}")
-    public ResponseEntity<DTORespostasListasCompromissoRecorrentes> listarCompromissosPorDiaDaSemana(@PathVariable DayOfWeek diadasemana){
-        DTORespostasListasCompromissoRecorrentes lista = compromissosRecorrentesService.buscarCompromissoPorDiaDaSemana(diadasemana);
-
-        return ResponseEntity.ok(lista);
-    }
-
     @GetMapping("/listarconflitos")
-    public ResponseEntity<List<List<DTOCompromissosRecorrentes>>> listarCompromissosConflitantes(){
-        List<List<DTOCompromissosRecorrentes>> lista = compromissosRecorrentesService.listarCompromissosConflitantes();
+    public ResponseEntity<List<List<DTOSaidaCompromissosRecorrentes>>> listarCompromissosConflitantes(){
+        List<List<DTOSaidaCompromissosRecorrentes>> lista = compromissosRecorrentesService.listarCompromissosConflitantes();
         return ResponseEntity.ok(lista);
     }
 
     @PostMapping("/criarcompromisso")
-    public ResponseEntity<DTORespostaCompromissoRecorrente> criarCompromisso(@RequestBody DTOCompromissosRecorrentes dtoCompromissosRecorrentes){
-        DTORespostaCompromissoRecorrente compromissoCriado = compromissosRecorrentesService.criarCompromisso(dtoCompromissosRecorrentes);
+    public ResponseEntity<DTORespostaCompromissoRecorrente> criarCompromisso(@RequestBody DTOCreateCompromissosRecorrentes dtoCreateCompromissosRecorrentes){
+        DTORespostaCompromissoRecorrente compromissoCriado = compromissosRecorrentesService.criarCompromissoRecorrente(dtoCreateCompromissosRecorrentes);
 
         return ResponseEntity.ok(compromissoCriado);
     }
 
     @PatchMapping("/alterarcompromisso/{id}")
     public ResponseEntity<DTORespostaCompromissoRecorrente> alterarCompromisso(@PathVariable long id, @RequestBody DTOUpdateCompromissosRecorrentes updateCompromissosRecorrentes){
-        DTORespostaCompromissoRecorrente compromissoAlterado = compromissosRecorrentesService.alterarCompromisso(id,updateCompromissosRecorrentes);
+        DTORespostaCompromissoRecorrente compromissoAlterado = compromissosRecorrentesService.alterarCompromissoRecorrente(id,updateCompromissosRecorrentes);
 
         return ResponseEntity.ok(compromissoAlterado);
     }
 
-    @DeleteMapping("/deletarcompromisso/{id}")
+    @PatchMapping("/adicionarhorarionocompromisso/{id}")
+    public ResponseEntity<DTOSaidaHorariosPorDia> adicionarHorario(@PathVariable Long compromissoRecorrenteId,
+                                                                   @RequestBody DTOCreateHorariosPorDia horariosPorDia){
+        DTOSaidaHorariosPorDia saidaHorariosPorDia = horariosPorDiaService.adicionarHorario(compromissoRecorrenteId,horariosPorDia);
+
+        return ResponseEntity.ok(saidaHorariosPorDia);
+    }
+
+    @PatchMapping("/alterarhorariodocompromisso/{compromissoid}/{idhorario}")
+    public ResponseEntity<DTOSaidaHorariosPorDia> alterarHorario(
+            @PathVariable("compromissoid") Long compromissoRecorrenteId,
+            @PathVariable("idhorario") Long horarioId,
+            @RequestBody DTOUpdateHorariosPorDia updateHorariosPorDia) {
+
+        DTOSaidaHorariosPorDia saidaHorariosPorDia = horariosPorDiaService.alterarHorario(compromissoRecorrenteId, horarioId, updateHorariosPorDia);
+        return ResponseEntity.ok(saidaHorariosPorDia);
+    }
+
+    @DeleteMapping("/deletarhorariodocompromisso/{id")
+    public ResponseEntity<List<DTOSaidaCompromissos>> deletarHorario(@PathVariable Long compromissoRecorrenteId,
+                                               @PathVariable Long horarioId){
+        List<DTOSaidaCompromissos> compromissosDeletados = horariosPorDiaService.deletarHorarioPorId(compromissoRecorrenteId,horarioId);
+
+        return ResponseEntity.ok(compromissosDeletados);
+    }
+
+    @DeleteMapping("/deletarcompromisso/{compromissoid}/{horarioid}")
     public ResponseEntity<Void> deletarcompromisso(@PathVariable long id){
         compromissosRecorrentesService.deletarCompromissoPorId(id);
 
