c190823986e42d4ab636681d804d1aab151638c0
Filipe Kisiolar <filipekisiolarfilho@terra.com.br>
2025-08-05 11:56:01 -0300

    refact: add the compromissos recorrentes tools and a system configuration at the chat client configuration to centralize the responsabilities and unauthorize questions about anithing that is not related to appoinmets

--- src/main/java/kisiolar/filipe/Viviane/Ai/IntegrationAI/ChatClientConfig.java
+++ src/main/java/kisiolar/filipe/Viviane/Ai/IntegrationAI/ChatClientConfig.java
@@ -7,8 +7,20 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class ChatClientConfig {
 
+    private final CompromissosRecorrentesTools compromissosRecorrentesTools;
+
+    public ChatClientConfig(CompromissosRecorrentesTools compromissosRecorrentesTools) {
+        this.compromissosRecorrentesTools = compromissosRecorrentesTools;
+    }
+
     @Bean
     public ChatClient chatClient(ChatClient.Builder builder){
-        return builder.build();
+        return builder
+                .defaultTools(compromissosRecorrentesTools)
+                .defaultSystem("voce é um assistente de compromissos " +
+                        "e só deve responder perguntas relacionadas a isso," +
+                        "caso haja alguma pergunta nao relacionada a isso responda " +
+                        "que so pode responder perguntas relacionadas a compromissos")
+                .build();
     }
 }
